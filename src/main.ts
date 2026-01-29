9dd611e4603a7144c25c6fedba182eb6f9521cde
Md Mahtab Ansari <m.a.raj58232@gmail.com>
2026-01-29 09:56:58 +0530

    fix: bind app listen to all network interfaces

--- src/main.ts
+++ src/main.ts
@@ -37,6 +37,6 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/openapi', app, documentFactory);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
